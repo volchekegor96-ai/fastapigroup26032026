8483c752c5125d228560843e8f449bb28f3985d1
“John <john@email.com>
2026-07-15 18:49:15 +0300

    fixed

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from fastapi import FastAPI
 from settings import settings
 from api_router import router as tour_router
 
-app = FastAPI(title="Tour Agency API")
+app = FastAPI(title="Tour Agеncy API")
 
 app.include_router(tour_router)
